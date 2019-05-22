5468a68d7a9e18a2f0f2ef416a739400f0c642a3
duchiha <39369441+duchiha@users.noreply.github.com>
2019-05-22 18:53:34 +0200

    flatMap -- how to use

---
+++ Stream API/flatMap_method/examples/UniqueCharacters.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class UniqueCharacters {
+    public static void main(String []args) {
+        String []string= "you never know what you have until you clean your room".split(" ");
+        Arrays.stream(string)
+                .flatMap(word -> Arrays.stream(word.split("")))
+                .distinct()
+                .forEach(System.out::print);
+    }
+}
