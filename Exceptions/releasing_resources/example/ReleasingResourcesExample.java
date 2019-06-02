8ebf83df761f3ee89eff2df7320841c9a2fd298e
duchiha <39369441+duchiha@users.noreply.github.com>
2019-06-02 17:10:47 +0200

    Add files via upload

---
+++ Exceptions/releasing_resources/example/ReleasingResourcesExample.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner; 
+
+class ReleasingResourcesExample {
+    public static void main(String [] args) {
+        System.out.println("Type an integer in the console: ");
+        Scanner consoleScanner = new Scanner(System.in); 
+	try { 
+	    System.out.println("You typed the integer value: " + consoleScanner.nextInt()); 
+	} catch(Exception e) {
+	    // call all other exceptions here ...
+            System.out.println("Error: Encountered an exception and could not read an integer from the console... "); 
+            System.out.println("Exiting the program - restart and try the program again!");
+	} finally {
+            System.out.println("Done reading the integer... closing the Scanner"); 
+	    consoleScanner.close(); 
+	}
+    }
+}
